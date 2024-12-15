fd80a6ae9146802388dfead15215b2dd8bb80983
thinkershine <siemaszkosebastian@gmail.com>
2024-12-15 12:22:06 +0100

    Fix Error

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -25,11 +25,13 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export async function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
+  /// This id WIll be seen in browset console...
+  /// super unsecure? but it's just ID...
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={id}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
